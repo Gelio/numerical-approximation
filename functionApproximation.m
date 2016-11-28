35cef3f1cb88ec7fb698ad1386c1418622d853f3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-11-28 21:38:04 +0100

    extract drawing plot to a separate function

--- functionApproximation.m
+++ functionApproximation.m
@@ -11,42 +11,10 @@ f = [10 11 10 5 100 14 2];
 % Ilosc punktow na wykresie
 pointsCount = 500;
 
+% Dodatkowy odstep od skrajnych punktow pomiaru, na ktorych tez zostanie
+% narysowany wykres
+paddingMultiplier = 0.2;
+
 % *Koniec konfiguracji*
 
-
-
-
-
-% Obliczenie elementow ukladu rownan G * alfa = F
-[G, F] = computeApproximationEquation(x, f);
-
-% Rozwiazanie ukladu rownan G * alfa = F
-alfa = computeApproximationCoefficients(G, F);
-
-% Sporzadzenie elementu optymalnego
-fOpt = composeApproximationFunction(alfa);
-
-
-% Przygotowanie argumentow do wykresu
-minX = min(x);
-maxX = max(x);
-% Powiekszenie obszaru, na ktorym obliczone beda wartosci funkcji
-% aproksymujacej
-distance = maxX - minX;
-padding = distance / 6;
-
-plotX = linspace(min(x) - padding, max(x) + padding, pointsCount);
-approximatedValues = fOpt(plotX);
-
-% Naniesienie wartosci na wykres
-figure;
-plot(x, f, 'r.', 'MarkerSize', 10);
-hold on;
-plot(plotX, approximatedValues);
-
-% Opis wykresu
-title('Aproksymacja dyskretna z baz¹ 1, x, x^2, sin x');
-legend('Punkty pomiarowe', 'Element optymalny (funkcja aproksymuj¹ca)');
-xlabel('x - argumenty');
-ylabel('y - wartoœci funkcji');
-xlim([min(plotX) max(plotX)]);
+plotApproximation(x, f, pointsCount, paddingMultiplier);
---
+++ plotApproximation.m
@@ -0,0 +1,62 @@
+% plotApproximation(x, f, pointsCount, paddingMultiplier) - funkcja
+% rysujaca wykres elementu optymalnego na podstawie punktow pomiaru (x, f).
+% Wartosci funkcji sa obliczane w pointsCount punktach. Dodatkowo mozna
+% okreslic odstep od skrajnych punktow pomiaru, na ktorych tez wartosci
+% zostana obliczone.
+%
+% Wejscie:
+% * x, f - wektory dlugosci m >= 4 zawierajace punkty pomiarowe
+% * pointsCount - ilosc punktow na ilu zostana obliczone wartosci do
+% wykresu (domyslnie 500)
+% * paddingMultiplier - odstep od skrajnych punktow pomiaru, na ktorych tez
+% wartosci zostana obliczone (domyslnie 0.2)
+%
+% Wyjscie:
+% brak, zostaje narysowany wykres
+%
+% Autor: Grzegorz Rozdzialik (D4, gr. lab. 2)
+
+function plotApproximation(x, f, pointsCount, paddingMultiplier)
+if nargin == 2
+    pointsCount = 500;
+end
+if nargin <= 3
+    paddingMultiplier = 0.1667;
+end
+
+% Obliczenie elementow ukladu rownan G * alfa = F
+[G, F] = computeApproximationEquation(x, f);
+
+% Rozwiazanie ukladu rownan G * alfa = F
+alfa = computeApproximationCoefficients(G, F);
+
+% Sporzadzenie elementu optymalnego
+fOpt = composeApproximationFunction(alfa);
+
+
+% Przygotowanie argumentow do wykresu
+minX = min(x);
+maxX = max(x);
+% Powiekszenie obszaru, na ktorym obliczone beda wartosci funkcji
+% aproksymujacej
+distance = maxX - minX;
+padding = distance * paddingMultiplier;
+
+plotX = linspace(min(x) - padding, max(x) + padding, pointsCount);
+approximatedValues = fOpt(plotX);
+
+% Naniesienie wartosci na wykres
+figure;
+plot(x, f, 'r.', 'MarkerSize', 10);
+hold on;
+plot(plotX, approximatedValues);
+
+% Opis wykresu
+title('Aproksymacja dyskretna z baz¹ 1, x, x^2, sin x');
+legend('Punkty pomiarowe', 'Element optymalny (funkcja aproksymuj¹ca)');
+xlabel('x - argumenty');
+ylabel('y - wartoœci funkcji');
+xlim([min(plotX) max(plotX)]);
+
+end
+
